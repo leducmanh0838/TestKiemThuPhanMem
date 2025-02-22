05287f2977c104f1674c2b20851bebfe192b7813
duc manh <2251012090manh@ou.edu.vn>
2025-02-22 16:35:32 +0700

    xu ly nut button 2

--- LeDucManh/Form1.cs
+++ LeDucManh/Form1.cs
@@ -21,5 +21,10 @@ namespace LeDucManh
         {
             String x = "Hello";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String x = "Button2";
+        }
     }
 }
